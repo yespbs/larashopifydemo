efdc394d7b20ac8a04508d6410fd0ad0d04dc6a3
Priyabrata Sarkar <yespbs@gmail.com>
2015-01-19 22:11:11 +0530

    first commit

--- app/larashopify/Services/OAuth2/Shopify.php
+++ app/larashopify/Services/OAuth2/Shopify.php
@@ -4,40 +4,43 @@ use League\OAuth2\Client\Provider\AbstractProvider;
 
 class Shopify extends AbstractProvider
 {
-/**
-* Store name.
-*
-* @var string
-*/
-public $store = '';
-/**
-* Autorize url.
-*
-* @return string
-*/
-public function urlAuthorize()
-{
-return sprintf('https://%s.myshopify.com/admin/oauth/authorize', $this->store);
-}
-/**
-* Access token url.
-*
-* @return string
-*/
-public function urlAccessToken()
-{
-return sprintf('https://%s.myshopify.com/admin/oauth/access_token', $this->store);
-}
-public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
-{
-return \URL::to('oauth/user'); // @TODO: extract this somehow
-}
-public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
-{
-try {
-return json_decode($response);
-} catch (\Exception $ex) {
-App::make('LifecycleMail\Repositories\UserRepository')->findByStoreOAuthToken($token['access_token']);
-}
-}
+	/**
+	* Store name.
+	*
+	* @var string
+	*/
+	public $store = 'larashopifydemo';
+	/**
+	* Autorize url.
+	*
+	* @return string
+	*/
+	public function urlAuthorize()
+	{
+		return sprintf('https://%s.myshopify.com/admin/oauth/authorize', $this->store);
+	}
+	
+	/**
+	* Access token url.
+	*
+	* @return string
+	*/
+	public function urlAccessToken()
+	{
+		return sprintf('https://%s.myshopify.com/admin/oauth/access_token', $this->store);
+	}
+
+	public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
+	{
+		return \URL::to('oauth/user'); // @TODO: extract this somehow
+	}
+
+	public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
+	{
+		try {
+			return json_decode($response);
+		} catch (\Exception $ex) {
+			App::make('LifecycleMail\Repositories\UserRepository')->findByStoreOAuthToken($token['access_token']);
+		}
+	}
 } 
