c11878cd0a692da8c64a940b0d65cdaf69a50622
Priyabrata Sarkar <yespbs@gmail.com>
2015-01-20 21:56:50 +0530

    update namespace

---
+++ app/larashopifydemo/Model/Store.php
@@ -0,0 +1,48 @@
+<?php
+namespace LaraShopifyDemo\Model;
+
+/**
+ * Store 
+ * 
+ * @package LaraShopify
+ * @subpackage Laravel
+ */
+class Store extends \Eloquent {
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'stores';
+
+	/**
+	 * Timestamp
+	 * 
+	 * @var bool
+	 */ 
+	public $timestamps = true;
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password');
+
+	/**
+	 * Fillable
+	 * 
+	 * @var array
+	 */ 
+	protected $fillable = ['store'];
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+}
